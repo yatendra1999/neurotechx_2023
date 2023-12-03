b183da950b1b9c2be89a81f32938653c5ac7a819
Yatendra Singh <yatendra@isrdsystems.com>
2023-12-03 09:36:56 +0530

    added StreamLSL replay

--- mne_test.py
+++ mne_test.py
@@ -5,14 +5,18 @@ from matplotlib import pyplot as plt
 from mne_lsl.datasets import sample
 from mne_lsl.lsl import local_clock
 from mne_lsl.stream import StreamLSL as Stream
+from mne_lsl.player import PlayerLSL as Player
 import mne
 import numpy as np
 from plot import DynamicPlotter
 
-# fname = sample.data_path() / "sample-ant-raw.fif"
-# player = Player(fname)
-# player.start()
-stream = Stream(bufsize=5, name="NEUPHONY", stype="EEG")  # 5 seconds of buffer
+import os
+
+dump_path = os.path.join(os.getcwd(), "dumps/40hz_freq.edf" )
+player = Player(dump_path)
+player.start()
+# stream = Stream(bufsize=5, name="NEUPHONY", stype="EEG")  # 5 seconds of buffer
+stream = Stream(bufsize=5, name=player.name)  # 5 seconds of buffer
 stream.connect(acquisition_delay=0.2)
 print(stream.info)
 
